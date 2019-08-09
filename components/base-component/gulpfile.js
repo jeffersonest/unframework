1a0596550609ab0eda4c07610894e7e9b7ef7f9e
JEFFERSON ESTEVAM <jesbmail@gmail.com>
2019-08-09 15:28:04 -0300

    Update gulpfile
    Add autoprefixer and watch task

--- components/base-component/gulpfile.js
+++ components/base-component/gulpfile.js
@@ -1,13 +1,25 @@
-var gulp = require('gulp');
-	var sass = require('gulp-sass');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var postcssPresetEnv = require("postcss-preset-env");
+var watch = require('gulp-watch');
 
-	//task para o sass
-	gulp.task('sass', function () {
-   	 return gulp.src('./dist/sass/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./dist/local_css'));
-	});
+//task para o sass
+gulp.task("sass", function() {
+  return gulp
+    .src("./dist/sass/*.scss")
+    .pipe(postcss([postcssPresetEnv(
+		{ 
+			browsers: 'last 1 versions',
+			autoprefixer: { grid: true } 
+		}
+	)]))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("./dist/local_css"));
+});
 
-	gulp.task('default', function(){
-		
-	});
+gulp.task('watch', function () {
+   gulp.watch('./dist/sass/main.scss', gulp.series('sass'));
+});
+
+gulp.task("default", function() {});
